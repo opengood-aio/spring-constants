4d4e385bfb60cc4d26b7d2df67cfc89a8724a8b6
Chris Jaehnen <cjaehnen@users.noreply.github.com>
2024-08-11 21:21:06 -0400

    Remove FOSSA badge

--- src/test/kotlin/io/opengood/constants/spring/SpringPropertySettingsTest.kt
+++ src/test/kotlin/io/opengood/constants/spring/SpringPropertySettingsTest.kt
@@ -3,9 +3,10 @@ package io.opengood.constants.spring
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-class SpringPropertySettingsTest : StringSpec({
+class SpringPropertySettingsTest :
+    StringSpec({
 
-    "SpringPropertySettings constant should return bean definition override property name" {
-        SpringPropertySettings.BEAN_DEFINITION_OVERRIDE shouldBe "spring.main.allow-bean-definition-overriding=true"
-    }
-})
+        "SpringPropertySettings constant should return bean definition override property name" {
+            SpringPropertySettings.BEAN_DEFINITION_OVERRIDE shouldBe "spring.main.allow-bean-definition-overriding=true"
+        }
+    })
